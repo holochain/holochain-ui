2eee7f24df271c3b590d490043acc95edd451d0c
Philip Beadle <philip.beadle@live.com.au>
2018-06-04 22:10:08 +1000

    Changed UI tests to use React server so routes work

--- ui-automation/cypress/integration/profiles.js
+++
@@ -1,5 +0,0 @@
-describe('How to work with Profiles', function () {
-  it('Show each Profile you have created', function () {
-    cy.request('POST', 'http://localhost:4141/fn/personas/personasList', '')
-  })
-})
