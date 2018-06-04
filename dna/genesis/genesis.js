a700249d3e439c8420b3da70c2f13c5a921dd105
Philip Beadle <philip.beadle@live.com.au>
2018-06-04 21:03:14 +1000

    Changed genesis to use app name instead of hash

--- dna/genesis/genesis.js
+++ dna/genesis/genesis.js
@@ -2,10 +2,21 @@ function genesis() {
   return true
 }
 
+function getDNA(channel){
+  var dnaList = getBridges()
+  // debug(dnaList)
+  var dnaEntry = dnaList.filter(function(dna){
+    return dna.CalleeName === channel
+  })
+  // debug(dnaEntry[0].CalleeApp)
+  return dnaEntry[0].CalleeApp
+}
+
 function bridgeGenesis(side, dna, appData) {
-  debug('holochain')
-  var Agent = JSON.parse(App.Agent.String.replace(/'/g, '"').slice(0, App.Agent.String.indexOf('}') + 1))
-  var genesisPublicKey = bridge(Agent.KeyMgtAppDNA, 'verify', 'getGenesisPublicKey', '')
-  var signature = bridge(Agent.KeyMgtAppDNA, 'verify', 'getSignature', '')
+  debug(App.Name + ' Bridged to: DNA: ' + dna)
+  var KeyMgtAppDNA = getDNA('holo-keys')
+  //var Agent = JSON.parse(App.Agent.String.replace(/'/g, '"').slice(0, App.Agent.String.indexOf('}') + 1))
+  var genesisPublicKey = bridge(KeyMgtAppDNA, 'verify', 'getGenesisPublicKey', '')
+  var signature = bridge(KeyMgtAppDNA, 'verify', 'getSignature', '')
   return verifySignature(signature, 'Revocation Method', genesisPublicKey)
 }
