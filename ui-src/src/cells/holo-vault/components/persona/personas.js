36aec5ef5afbbceb29deaa4bb83b3fcd1b19ab73
Philip Beadle <philip.beadle@live.com.au>
2018-08-23 10:09:35 +1000

    Removed unused vars

--- ui-src/src/cells/holo-vault/components/persona/personas.js
+++ ui-src/src/cells/holo-vault/components/persona/personas.js
@@ -5,9 +5,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
 import Typography from '@material-ui/core/Typography';
 import {Route} from 'react-router-dom'
 import Button from '@material-ui/core/Button';
