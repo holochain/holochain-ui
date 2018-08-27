e38943cf48ec24cfb4004cd4a9c31993a1d58dfd
Willem Olding <willemolding@gmail.com>
2018-08-27 17:04:00 +1000

    changes storyboard config to match js and ts stories. Both are working side-by-side

--- ui-src/.storybook/config.js
+++ ui-src/.storybook/config.js
@@ -6,7 +6,7 @@ addDecorator(withKnobs);
 setAddon(JSXAddon);
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../src", true, /.stories.tsx$/);
+const req = require.context("../src", true, /.(stories|story).(tsx|ts|js|jsx)$/);
 function loadStories() {
   require("./welcomeStory");
   req.keys().forEach(filename => req(filename));
