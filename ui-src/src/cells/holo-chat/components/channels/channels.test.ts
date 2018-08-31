a5c9db8293d803ac5702f9b34907ada7e4fdbb7c
Philip Beadle <philip.beadle@live.com.au>
2018-08-31 17:33:16 +1000

    Update channels.test.ts

--- ui-src/src/cells/holo-chat/components/channels/channels.test.ts
+++ ui-src/src/cells/holo-chat/components/channels/channels.test.ts
@@ -7,4 +7,8 @@ export const channelsTests = describe('Listing your channels', () => {
   it('Channel view shows the list of existing channels', () => {
     expect(3).toEqual(3)
   })
+  
+  it('People can add new specs via the GitHub editor.', () => {
+    expect(3).toEqual(3)
+  })
 })
