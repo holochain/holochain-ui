39565f08033952ccaf01c8b11d027e7200989e32
Willem Olding <willemolding@gmail.com>
2018-09-04 14:53:54 +1000

    building with merge from upstream

--- ui-src/src/cells/holo-chat/containers/ideaContainer.ts
+++ ui-src/src/cells/holo-chat/containers/ideaContainer.ts
@@ -4,8 +4,11 @@ import IdeaCard from '../components/cards/ideaCard'
 import  * as constants from '../constants'
 import {HoloChatState} from '../reducer'
 import {Dispatch} from 'redux'
+import {addCard} from '../../errand/actions'
 import {
 } from '../actions'
+
+// declare function addCard(card_info: any, lane_id: any): AnyAction
 
 const mapStateToProps = (state: HoloChatState) => {
   return {
@@ -15,9 +18,9 @@ const mapStateToProps = (state: HoloChatState) => {
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
-    // addCard: (card_info, lane_id) => {
-    //   dispatch(addCard(card_info, lane_id))
-    // }
+    addCard: (card_info: any, lane_id: string) => {
+      dispatch(addCard(card_info, lane_id))
+    }
   }
 }
 
