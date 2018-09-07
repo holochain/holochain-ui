d1ce4c130d7606c63a0d30de94d3f3f0f25c719f
Willem Olding <willemolding@gmail.com>
2018-09-07 22:09:35 +1000

    automated tests working for actions generated from table

--- ui-src/src/cells/holo-chat/actions.test.ts
+++ ui-src/src/cells/holo-chat/actions.test.ts
@@ -1,5 +1,6 @@
 import configureMockStore, {MockStoreEnhanced} from 'redux-mock-store'
 import axios from 'axios'
+import {AnyAction} from 'redux'
 // @ts-ignore
 import axiosMiddleware from 'redux-axios-middleware'
 import MockAdapter from 'axios-mock-adapter'
@@ -19,7 +20,6 @@ let mock: MockAdapter
 beforeEach(() => {
 	store = mockStore(initialState)
 	mock = new MockAdapter(mockHolochainClient);
-	mock.onPost('/').reply(200, 'channel-hash-12345')
 })
 
 afterEach(() => {
@@ -42,64 +42,60 @@ function genExpectedAction(fname: string, data: any): any {
 	}
 }
 
+const asyncActionTestTable: Array<[string, (input: any) => AnyAction, any, any]> = [
+	[
+		'createCustomChannel', 
+		chatActions.createCustomChannel, 
+		{name: 'test channel', description: '', members: ['123abc']}, 
+		'channel-hash-12345'
+	],
+	[
+		'addMembers', 
+		chatActions.addMembers, 
+		{channelHash: 'Qmchannelhash', members: ['123abc']}, 
+		true
+	],
+	[
+		'getMyChannels', 
+		chatActions.getMyChannels, 
+		{}, 
+		[{name: 'channel1', members: ['member1']}]
+	],
+	[
+		'getMembers', 
+		chatActions.getMembers, 
+		'Qmchannelhash', 
+		[{handle: 'member'}]
+	],
+	[
+		'postMessage', 
+		chatActions.postMessage, 
+		{channelHash: 'Qmchanelhash', message: {content:{text:'message body'}}}, 
+		'Qmmessagehash'
+	]
+]
 
-describe('createCustomChannel action', () => {
+asyncActionTestTable.forEach(([name, actionCreator, testInput, testResponse]) => {
 
-	const testInput = {name: 'test channel', description: '', members: ['123abc']}
-	const expectedAction = genExpectedAction('createCustomChannel', testInput)
+	describe(`${name} action`, () => {
 
-	it('should create an action that is correctly structured given parameters', () => {
-		expect(chatActions.createCustomChannel(testInput)).toEqual(expectedAction)
+		const expectedAction = genExpectedAction(name, testInput)
+
+		it('should create an action that is correctly structured given parameters', () => {
+			expect(actionCreator(testInput)).toEqual(expectedAction)
+		})
+
+		it('should trigger middleware creating a request response and correctly modify the state', () => {
+			mock.onPost('/').reply(200, testResponse)
+		    // @ts-ignore - minor error in the typings for redux/typesafe-actions
+		    return store.dispatch(actionCreator(testInput)).then(() => {
+				const actions = store.getActions()
+				expect(actions[0]).toEqual(expectedAction)
+				expect(store.getActions()[1].payload.data).toEqual(testResponse)
+		    })
+		})
 	})
 
-	it('should trigger middleware creating a request response', () => {
-	    // @ts-ignore - minor error in the typings for redux/typesafe-actions
-	    return store.dispatch(chatActions.createCustomChannel({name: 'test channel', description: '', members: ['123abc']})).then(() => {
-			const actions = store.getActions()
-			expect(actions[0]).toEqual(expectedAction)
-			expect(store.getActions()[1].type).toEqual('holochat/createCustomChannel_SUCCESS')
-			expect(store.getActions()[1].payload.data).toEqual('channel-hash-12345')
-			expect(store.getState()).toEqual(initialState)
-	    })
-
-	})
 })
 
 
-describe('getMyChannels action', () => {
-
-	const expectedAction = {
-		type: 'holochat/getMyChannels',
-		payload: {
-			request: {
-				data: {
-					channel: 'holo-chat',
-					zome: 'custom_channel',
-					func: 'getMyChannels',
-					data: {}
-				}
-			}
-		}
-	}
-
-
-	it('should produce the correct action type', () => {
-		expect(chatActions.getMyChannels()).toEqual(expectedAction)
-	})
-
-	it('should trigger middleware and create the correct response', () => {
-		mock.onPost('/').reply(200, ['channel1', 'channel2'])
-	})
-
-	const store = mockStore({})
-    // @ts-ignore - minor error in the typings for redux/typesafe-actions
-    return store.dispatch(chatActions.getMyChannels()).then(() => {
-      // return of async actions
-      const actions = store.getActions()
-      expect(actions[0]).toEqual(expectedAction)
-      expect(store.getActions()[1].type).toEqual('holochat/getMyChannels_SUCCESS')
-      expect(store.getActions()[1].payload.data).toEqual(['channel1', 'channel2'])
-    })
-})
-
-// TODO: write tests for other action creators
