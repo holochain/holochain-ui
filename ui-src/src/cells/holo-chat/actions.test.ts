6b808b960acee8de969f365d01875124a2637cc4
Willem Olding <willemolding@gmail.com>
2018-09-07 21:01:03 +1000

    refactors ui action tests. passing

--- ui-src/src/cells/holo-chat/actions.test.ts
+++ ui-src/src/cells/holo-chat/actions.test.ts
@@ -1,9 +1,10 @@
-import configureMockStore from 'redux-mock-store'
+import configureMockStore, {MockStoreEnhanced} from 'redux-mock-store'
 import axios from 'axios'
 // @ts-ignore
 import axiosMiddleware from 'redux-axios-middleware'
 import MockAdapter from 'axios-mock-adapter'
 import * as chatActions from './actions'
+import {initialState} from './reducer'
 
 const mockHolochainClient = axios.create({
 	baseURL: '/fn/holochain/callBridgedFunction',
@@ -12,71 +13,61 @@ const mockHolochainClient = axios.create({
 })
 
 const mockStore = configureMockStore([axiosMiddleware(mockHolochainClient)])
+let store: MockStoreEnhanced
+let mock: MockAdapter
 
+beforeEach(() => {
+	store = mockStore(initialState)
+	mock = new MockAdapter(mockHolochainClient);
+	mock.onPost('/').reply(200, 'channel-hash-12345')
+})
 
-describe('createCustomChannel action', () => {
-	let mock: MockAdapter
-	const expectedAction = {
-		type: 'holochat/createCustomChannel',
+afterEach(() => {
+	mock.reset()
+})
+
+function genExpectedAction(fname: string, data: any): any {
+	return {
+		type: 'holochat/'+fname,
 		payload: {
 			request: {
 				data: {
 					channel: 'holo-chat',
 					zome: 'custom_channel',
-					func: 'createCustomChannel',
-					data: {members: ['123abc']}
+					func: fname,
+					data: data
 				}
 			}
 		}
 	}
-	beforeEach(() => {
-		mock = new MockAdapter(mockHolochainClient);
-	})
+}
 
-	afterEach(() => {
-		mock.reset()
-	})
+
+describe('createCustomChannel action', () => {
+
+	const testInput = {name: 'test channel', description: '', members: ['123abc']}
+	const expectedAction = genExpectedAction('createCustomChannel', testInput)
 
 	it('should create an action that is correctly structured given parameters', () => {
-
-		expect(chatActions.createCustomChannel({name: 'test channel', description: '', members: ['123abc']})).toEqual(expectedAction)
+		expect(chatActions.createCustomChannel(testInput)).toEqual(expectedAction)
 	})
 
-
 	it('should trigger middleware creating a request response', () => {
-
-		mock.onPost('/').reply(200, 'CHANNEL_UUID_000')
-
-		const expectedAction = {
-			type: 'holochat/createCustomChannel',
-			payload: {
-				request: {
-					data: {
-						channel: 'holo-chat',
-						zome: 'custom_channel',
-						func: 'createCustomChannel',
-						data: {name: 'test channel', description: '', members: ['123abc']}
-					}
-				}
-			}
-		}
-
-    const store = mockStore({})
-    // @ts-ignore - minor error in the typings for redux/typesafe-actions
-    return store.dispatch(chatActions.createCustomChannel(['123abc'])).then(() => {
-      // return of async actions
-      const actions = store.getActions()
-      expect(actions[0]).toEqual(expectedAction)
-      expect(store.getActions()[1].type).toEqual('holochat/createCustomChannel_SUCCESS')
-      expect(store.getActions()[1].payload.data).toEqual('CHANNEL_UUID_000')
-    })
+	    // @ts-ignore - minor error in the typings for redux/typesafe-actions
+	    return store.dispatch(chatActions.createCustomChannel({name: 'test channel', description: '', members: ['123abc']})).then(() => {
+			const actions = store.getActions()
+			expect(actions[0]).toEqual(expectedAction)
+			expect(store.getActions()[1].type).toEqual('holochat/createCustomChannel_SUCCESS')
+			expect(store.getActions()[1].payload.data).toEqual('channel-hash-12345')
+			expect(store.getState()).toEqual(initialState)
+	    })
 
 	})
 })
 
 
 describe('getMyChannels action', () => {
-	let mock: MockAdapter
+
 	const expectedAction = {
 		type: 'holochat/getMyChannels',
 		payload: {
@@ -91,16 +82,8 @@ describe('getMyChannels action', () => {
 		}
 	}
 
-	beforeEach(() => {
-		mock = new MockAdapter(mockHolochainClient);
-	})
-
-	afterEach(() => {
-		mock.reset()
-	})
 
 	it('should produce the correct action type', () => {
-
 		expect(chatActions.getMyChannels()).toEqual(expectedAction)
 	})
 
