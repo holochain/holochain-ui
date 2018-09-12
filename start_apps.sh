b188fad5fa1b3e77cbee447c99ad75acfac44d7f
Willem Olding <willemolding@gmail.com>
2018-09-12 12:55:19 +1000

    tries to simplify docker setup. Cannot get bridges to connect with this method

--- start_apps.sh
+++ start_apps.sh
@@ -7,11 +7,11 @@ hcadmin init $AGENTID
 hcadmin join . base
 hcadmin join ./services/holo-chat chat
 
-jq ".DHTPort = $((6283 + $PORTOFFSET))" ~/.holochain/base/config.json | sponge ~/.holochain/base/config.json
-jq ".BootstrapServer = \"$BOOSTRAPSERVER\"" ~/.holochain/base/config.json | sponge ~/.holochain/base/config.json
+# jq ".DHTPort = $((6283 + $PORTOFFSET))" ~/.holochain/base/config.json | sponge ~/.holochain/base/config.json
+# jq ".BootstrapServer = \"$BOOSTRAPSERVER\"" ~/.holochain/base/config.json | sponge ~/.holochain/base/config.json
 
-jq ".DHTPort = $((7283 + $PORTOFFSET))" ~/.holochain/chat/config.json | sponge ~/.holochain/chat/config.json
-jq ".BootstrapServer = \"$BOOSTRAPSERVER\"" ~/.holochain/chat/config.json | sponge ~/.holochain/chat/config.json
+# jq ".DHTPort = $((7283 + $PORTOFFSET))" ~/.holochain/chat/config.json | sponge ~/.holochain/chat/config.json
+# jq ".BootstrapServer = \"$BOOSTRAPSERVER\"" ~/.holochain/chat/config.json | sponge ~/.holochain/chat/config.json
 
 hcadmin bridge base chat genesis
 
