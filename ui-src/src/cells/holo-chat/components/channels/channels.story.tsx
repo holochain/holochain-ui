5a75ec19c5c0f0d26f17aa9b78ae14ff1a121372
Philip Beadle <philip.beadle@live.com.au>
2018-09-17 12:32:47 +1000

    Styled the dialog

--- ui-src/src/cells/holo-chat/components/channels/channels.story.tsx
+++ ui-src/src/cells/holo-chat/components/channels/channels.story.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-// import {Provider} from 'react-redux'
+import {Provider} from 'react-redux'
 import { storiesOf } from "@storybook/react";
 import { MemoryRouter } from 'react-router'
 import { withNotes } from '@storybook/addon-notes'
@@ -10,11 +10,11 @@ import newChannel from './newChannel.md'
 import filterAgents from './filterAgents.md'
 import selectAgent from './selectAgent.md'
 import newChat from './newChat.md'
-// import CreateStore from '../../../../store'
+import CreateStore from '../../../../store'
 // import  * as constants from '../../constants'
 // import{Message as MessageType} from '../../types/message'
 import Channels from './channels'
-// import NewChannel from './newChannel'
+import NewChannel from './newChannel'
 
 import {specs} from 'storybook-addon-specifications'
 import { newChannelTests } from './newChannel.test'
@@ -25,7 +25,7 @@ import { channelsTests } from './channels.test'
 import channelData from '../../data/channels.json'
 
 configure({adapter: new Adapter()})
-// let store = CreateStore()
+let store = CreateStore()
 
 function StartComponent () {
     return (
@@ -40,11 +40,11 @@ storiesOf('HoloChat/Channels', module)
   // .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add('List my Channels', withNotes(listChannels) (() => {
     specs(() => channelsTests)
-      return <Channels channels={channelData} />
+    return <Provider store={store}><MemoryRouter initialEntries={['/']}><Channels channels={channelData} /></MemoryRouter></Provider>
   }))
   .add('Start a new Channel', withNotes(newChannel) (() => {
     specs(() => newChannelTests)
-      return <StartComponent />
+      return <NewChannel open={true} users={[{hash: '12334', handle:'wollum', avatar:''}, {hash: '1233', handle:'Sarah', avatar:''}, {hash: '1234', handle:'Nicksmith', avatar:''}]}/>
   }))
   .add('Filter the list', withNotes(filterAgents) (() => {
     specs(() => filterAgentsTests)
