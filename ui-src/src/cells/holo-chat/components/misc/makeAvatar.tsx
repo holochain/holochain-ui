24adec985077ba748d4f1218b00622906c80550e
Willem Olding <willemolding@gmail.com>
2018-09-20 10:39:13 +1000

    avatar renders a base64 image from vault

--- ui-src/src/cells/holo-chat/components/misc/makeAvatar.tsx
+++ ui-src/src/cells/holo-chat/components/misc/makeAvatar.tsx
@@ -5,11 +5,11 @@ import {Identity} from '../../types/model/identity'
 
 // used to check if the avatar string is valid base64 encoding.
 // Does not check if it decodes to a valid image
-const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+// const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
 export const MakeAvatar = (props: {user: Identity}) => {
   const {user} = props
-  if(user.avatar && base64regex.test(user.avatar)) {
+  if(user.avatar && user.avatar.length > 0) {
     return (<Avatar src={user.avatar}/>)
   } else if(user.handle) {
     return (<Avatar>{user.handle[0]}</Avatar>)
