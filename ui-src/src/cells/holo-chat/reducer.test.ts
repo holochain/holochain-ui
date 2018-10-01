67ae907ec6f4c80ea5ffbfbdd23ec53778840efc
Willem Olding <willemolding@gmail.com>
2018-10-01 16:05:01 +1000

    adds tests for reducing getMessages and getMembers

--- ui-src/src/cells/holo-chat/reducer.test.ts
+++ ui-src/src/cells/holo-chat/reducer.test.ts
@@ -3,10 +3,11 @@ import { getType } from 'typesafe-actions';
 import { AxiosResponse } from 'axios'
 
 import * as chatActions from './actions'
+import { MessageType } from './types/message'
 
 describe('Chat Reducer', () => {
 
-	it('Should update the state when a getMyChannels success action is received', () => {
+	it('Should update the state in response to GetMyChannels', () => {
 		expect(holochatReducer(undefined, {
 			type: getType(chatActions.GetMyChannels.success),
 			payload: {
@@ -17,4 +18,29 @@ describe('Chat Reducer', () => {
 			myChannels: ['channel1', 'channel2']
 		})
 	})
+
+	it('Should update the state in response to GetMessages', () => {
+		expect(holochatReducer(undefined, {
+			type: getType(chatActions.GetMessages.success),
+			payload: {
+				data: [{type: MessageType.CHAT, author: 'xxx', timestamp: 0, content: '', replies: [], channelId: ''}]
+			} as AxiosResponse
+		})).toEqual({
+			...initialState,
+			currentMessages: [{type: MessageType.CHAT, author: 'xxx', timestamp: 0, content: '', replies: [], channelId: ''}]
+		})
+	})
+
+	it('Should update the state in response to GetMembers', () => {
+		expect(holochatReducer(undefined, {
+			type: getType(chatActions.GetMembers.success),
+			payload: {
+				data: [{handle: 'a', avatar: ''}, {handle: 'b', avatar: ''}]
+			} as AxiosResponse
+		})).toEqual({
+			...initialState,
+			activeChannelMembers: [{handle: 'a', avatar: ''}, {handle: 'b', avatar: ''}]
+		})
+	})
+
 })
