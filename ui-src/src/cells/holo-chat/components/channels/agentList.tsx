1f8ba58f14b3a0d54ec3d682c978990a803b18bb
Micah J <micalangelone@gmail.com>
2018-10-18 15:30:09 -0400

    Testing adding Downshift
    From Material UI https://material-ui.com/demos/autocomplete/#downshift

--- ui-src/src/cells/holo-chat/components/channels/agentList.tsx
+++ ui-src/src/cells/holo-chat/components/channels/agentList.tsx
@@ -8,7 +8,14 @@ import ListItemText from '@material-ui/core/ListItemText'
 import withRoot from '../../../../withRoot'
 import { MakeAvatar } from '../misc/makeAvatar'
 // import {Route} from 'react-router-dom'
-
+import deburr from 'lodash/deburr';
+import keycode from 'keycode';
+import Downshift from 'downshift';
+import TextField from '@material-ui/core/TextField';
+import Popper from '@material-ui/core/Popper';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
 import { Identity } from '../../types/model/identity'
 
 const styles: StyleRulesCallback = (theme: Theme) => ({
@@ -77,7 +84,101 @@ class AgentList extends React.Component<AgentListProps, AgentListState> {
     }
   }
 
-  render () {
+  // handleKeyDown = event => {
+  //   const { filterString, selectedUsers } = this.state;
+  //   if (selectedUsers.length && !filterString.length && keycode(event) === 'backspace') {
+  //     this.setState({
+  //       selectedUsers: selectedUsers.slice(0, selectedUsers.length - 1),
+  //     });
+  //   }
+  // };
+  //
+  // handleInputChange = event => {
+  //   this.setState({ inputValue: event.target.value });
+  // };
+  //
+  // handleChange = user => {
+  //   let { selectedUsers } = this.state;
+  //
+  //   if (selectedUsers.indexOf(user) === -1) {
+  //     selectedUsers = [...selectedUsers, user];
+  //   }
+  //
+  //   this.setState({
+  //     filterString: '',
+  //     selectedUsers,
+  //   });
+  // };
+  //
+  // handleDelete = user => () => {
+  //   this.setState(state => {
+  //     const selectedUsers = [...state.selectedUsers];
+  //     selectedUsers.splice(selectedUsers.indexOf(user), 1);
+  //     return { selectedUsers };
+  //   });
+  // };
+  //
+  // render() {
+  //   const { classes } = this.props;
+  //   const { inputValue, selectedItem } = this.state;
+  //
+  //   return (
+  //     <Downshift
+  //       id="downshift-multiple"
+  //       inputValue={inputValue}
+  //       onChange={this.handleChange}
+  //       selectedItem={selectedItem}
+  //     >
+  //       {({
+  //         getInputProps,
+  //         getItemProps,
+  //         isOpen,
+  //         inputValue: inputValue2,
+  //         selectedItem: selectedItem2,
+  //         highlightedIndex,
+  //       }) => (
+  //         <div className={classes.container}>
+  //           {renderInput({
+  //             fullWidth: true,
+  //             classes,
+  //             InputProps: getInputProps({
+  //               startAdornment: selectedItem.map(item => (
+  //                 <Chip
+  //                   key={item}
+  //                   tabIndex={-1}
+  //                   label={item}
+  //                   className={classes.chip}
+  //                   onDelete={this.handleDelete(item)}
+  //                 />
+  //               )),
+  //               onChange: this.handleInputChange,
+  //               onKeyDown: this.handleKeyDown,
+  //               placeholder: 'Select multiple countries',
+  //             }),
+  //             label: 'Label',
+  //           })}
+  //           {isOpen ? (
+  //             <Paper className={classes.paper} square>
+  //               {getSuggestions(inputValue2).map((suggestion, index) =>
+  //                 renderSuggestion({
+  //                   suggestion,
+  //                   index,
+  //                   itemProps: getItemProps({ item: suggestion.label }),
+  //                   highlightedIndex,
+  //                   selectedItem: selectedItem2,
+  //                 }),
+  //               )}
+  //             </Paper>
+  //           ) : null}
+  //         </div>
+  //       )}
+  //     </Downshift>
+  //   );
+  // }
+
+
+
+   render () {
     const { classes, users } = this.props
     return (
       <div className={classes.root}>
