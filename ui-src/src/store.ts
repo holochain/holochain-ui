34d5ac0eed69ec016002c6105f40c4cd36a02d72
Willem Olding <willemolding@gmail.com>
2018-10-30 17:27:10 +1100

    adds working calls to rust!

--- ui-src/src/store.ts
+++ ui-src/src/store.ts
@@ -1,14 +1,20 @@
-
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import holoVault from './cells/holo-vault/reducer'
 import holoChat from './cells/holo-chat/reducer'
+// @ts-ignore
+import axiosMiddleware from 'redux-axios-middleware'
+import axios from 'axios'
 import errand from './cells/errand/reducer'
-import promiseMiddleware from 'redux-promise-middleware'
+
+const holochainClient = axios.create({
+  baseURL: '/call',
+  method: 'POST'
+})
 
 let rootReducer = combineReducers({ holoVault: holoVault, holoChat: holoChat, errand: errand, form: formReducer })
 
-const middleware = [promiseMiddleware()]
+const middleware = [axiosMiddleware(holochainClient)]
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
