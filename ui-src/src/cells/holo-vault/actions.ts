8c61f9dddff36d74a19267cc11233a0171c88e61
Willem Olding <willemolding@gmail.com>
2018-10-30 17:11:51 +1100

    update holochain axios for including capabilities

--- ui-src/src/cells/holo-vault/actions.ts
+++ ui-src/src/cells/holo-vault/actions.ts
@@ -1,6 +1,5 @@
 
 import { createHolochainAsyncAction } from '../../utils/holochainAxiosActions'
-import { createHolochainAsyncAction as createHolochainAsyncActionWs } from '../../utils/holochainWebsocketActions'
 
 import { Persona, PersonaSpec, PersonaField } from './types/persona'
 import { ProfileField, ProfileMapping, Profile } from './types/profile'
@@ -9,18 +8,18 @@ type Hash = string
 
 /*----------  Persona Actions  ----------*/
 
-export const CreatePersona = createHolochainAsyncAction<PersonaSpec, string>('holo-vault', 'personas', 'createPersona')
+export const CreatePersona = createHolochainAsyncAction<PersonaSpec, string>('holo-vault', 'personas', 'main', 'createPersona')
 
-export const GetPersonas = createHolochainAsyncActionWs<any, Array<Persona>>('XXXXXX', 'personas', 'main', 'get_personas')
+export const GetPersonas = createHolochainAsyncAction<any, Array<Persona>>('XXXXXX', 'personas', 'main', 'get_personas')
 
-export const AddField = createHolochainAsyncAction<{personaHash: string, field: PersonaField}, boolean>('holo-vault', 'personas', 'addField')
+export const AddField = createHolochainAsyncAction<{personaHash: string, field: PersonaField}, boolean>('holo-vault', 'personas', 'main', 'addField')
 
-export const DeleteField = createHolochainAsyncAction< {personaHash: string, fieldName: string}, number>('holo-vault', 'personas', 'deleteField')
+export const DeleteField = createHolochainAsyncAction< {personaHash: string, fieldName: string}, number>('holo-vault', 'personas', 'main', 'deleteField')
 
 /*----------  Profile Actions  ----------*/
 
-export const GetProfiles = createHolochainAsyncAction<any, Array<Profile>>('holo-vault', 'profiles', 'getProfiles')
+export const GetProfiles = createHolochainAsyncAction<any, Array<Profile>>('holo-vault', 'profiles', 'main', 'getProfiles')
 
-export const CreateMapping = createHolochainAsyncAction<ProfileMapping, number>('holo-vault', 'profiles', 'createMapping')
+export const CreateMapping = createHolochainAsyncAction<ProfileMapping, number>('holo-vault', 'profiles', 'main', 'createMapping')
 
-export const GetProfileFields = createHolochainAsyncAction<Hash, Array<ProfileField>>('holo-vault', 'profiles', 'getProfileFields')
+export const GetProfileFields = createHolochainAsyncAction<Hash, Array<ProfileField>>('holo-vault', 'profiles', 'main', 'getProfileFields')
