051451a9e6de6048c65b7bab6b9063c9335b84e5
Willem Olding <willemolding@gmail.com>
2018-12-04 15:00:28 +1100

    adds middleware bt cant get access to call function

---
+++ ui-src/src/utils/hcMiddleware.js
@@ -0,0 +1,35 @@
+
+import { connect } from './hc-web-client'
+
+export const holochain = (url) => store => {
+	// stuff here has the same life as the store!
+	// this is how we persist a websocket connection
+	let call = null
+
+	connect(url).then(({call}) => {
+		store.dispatch({type: 'HOLOCHAIN_WEBSOCKET_CONNECTED', payload: url})
+		call = call
+	})
+
+	return next => action => {
+		// send off the original action for debug
+		next(action)
+
+		if (action.payload && action.payload.holochainAction) {
+			if(call) {
+				return new Promise(resolve => store.dispatch({type: action.type+'SUCCESS', payload: 'A connection available'}))			
+				// return call(happ, zome, capability, func)(params)
+				// .then((stringResult) => {
+				// 	result = JSON.parse(stringResult)
+				// 	return store.dispatch({type: action.type+'_SUCCESS', payload: result})
+				// })
+				// .catch((err) => {
+	  	// 			return store.dispatch({type: action.type+'_FAILURE', payload: err})
+				// })
+			} else {
+	  			return new Promise(resolve => store.dispatch({type: action.type+'_FAILURE', payload: 'No connection available'}))			
+			}
+		}
+	}
+
+} 
