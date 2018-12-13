9bdfc07682a5ee99fa0d7bedc022303304809039
Jean Russell <thrivable@Jeans-MacBook-Pro.local>
2018-12-12 20:58:38 -0600

    added new story for mobile reply

--- ui-src/src/cells/holo-chat/components/messages/messages.stories.tsx
+++ ui-src/src/cells/holo-chat/components/messages/messages.stories.tsx
@@ -8,7 +8,8 @@ import * as Adapter from 'enzyme-adapter-react-16'
 import Messages from './messages'
 import streamMessagesDesktop from './streamMessagesDesktop.md'
 import streamMessagesMobile from './streamMessagesMobile.md'
-import reply from './reply.md'
+import reply Desktop from './replyDesktop.md'
+import reply Mobile from './replyMobile.md'
 import CreateStore from '../../../../store'
 import * as constants from '../../constants'
 import { Message as MessageType } from '../../types/model/message'
@@ -34,7 +35,10 @@ storiesOf('HoloChat/Messages', module)
   .add('Stream mobile', withNotes(streamMessagesMobile)(() => {
     return getMessages(constants.messages)
   }))
-  .add('Reply', withNotes(reply)(() => {
+  .add('Reply desktop', withNotes(replyDesktop)(() => {
+    return <StartComponent />
+  }))
+  .add('Reply mobile', withNotes(replyMobile)(() => {
     return <StartComponent />
   }))
 
