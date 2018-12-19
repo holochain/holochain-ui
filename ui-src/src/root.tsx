35db4e20ad1086b443ea115364540414de36bc21
Willem Olding <willemolding@gmail.com>
2018-12-20 10:03:17 +1100

    adds back side bar and adds a third agent to config

--- ui-src/src/root.tsx
+++ ui-src/src/root.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 // import Navigation from './skins/nav'
-import DesktopChat from './hApps/holo-chat/components/desktopChat'
+// import DesktopChat from './hApps/holo-chat/components/desktopChat'
+import Home from './skins/home'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const Root = ({ store }: {store: any}) => (
   <Provider store={store}>
     <Router>
-      <Route path='/' component={DesktopChat} />
+      <Route path='/' component={Home} />
     </Router>
   </Provider>
 )
